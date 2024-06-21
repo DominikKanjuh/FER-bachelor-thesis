7252eaebca8bc1329516e1be1f2496fdab7e998a
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2024-06-21 04:34:00 +0200

    search on dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,55 @@
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import { ArrowUpDownIcon } from 'lucide-react';
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-4 md:p-6">
+      <div className="flex flex-col gap-4 md:flex-row md:items-center mb-8">
+        <div className="flex-1">
+          <Input placeholder="Search CVs by title..." />
+        </div>
+        <div className="flex justify-end items-center gap-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="gap-2">
+                <ArrowUpDownIcon className="w-4 h-4" />
+                Sort by
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuRadioGroup value="date_created">
+                <DropdownMenuRadioItem value="date_created">Date Created</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="date_modified">Date Modified</DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="gap-2">
+                <ArrowUpDownIcon className="w-4 h-4" />
+                From
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuRadioGroup value="new_to_old">
+                <DropdownMenuRadioItem value="new_to_old">New to Old</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="old_to_new">Old to New</DropdownMenuRadioItem>
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
